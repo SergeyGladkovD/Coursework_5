81414d8600185af7a55ce341f76a096fea0c9b98
Gladkov Sergey <melcor2006@mail.ru>
2024-04-05 23:13:04 +0300

    mid work

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def go():
-	print('Надо с чего то начать.')
+from src.api import HeadHunterAPI
+from src.config import config
+from src.dbm import DBManager
 
 
-if __name__ == "__main__":
-	go()
+def new_works():
+	while True:
+		user_input = input(f'Хотите посмотреть созданную таблицу вакансий нажмите 1\n'
+                           f'Хотите удалит созданную таблицу и начать новый поиск вакансий нажмите 2\n'
+                           f'Остальные команды прекратят работу программы.\n')
+		if user_input == '1':
+			print("Тут надо прописать логику и запуск bdm команд.")
+			break
+		elif user_input == '2':
+			params = config()
+			DBManager.create_database(params)
+			DBManager.create_table(params)
+			vacancy = input('Какую вакансию вы ищете?\n')
+			city = input('В каком городе?\n')
+			data = HeadHunterAPI.get_vacancies(vacancy, city)
+			DBManager.add_data_in_bd(data, params)
+
+		else:
+			print('Программа завершена.')
+		break
+
+
+if __name__ == '__main__':
+	new_works()
---
+++ src/api.py
@@ -0,0 +1,15 @@
+import requests
+
+
+class HeadHunterAPI:
+	@staticmethod
+	def get_vacancies(vacancy: str, city: str) -> dict:
+		""" Получение вакансий с hh.ru в формате JSON. """
+		url = 'https://api.hh.ru/vacancies'
+		params = {
+			'text': f'{vacancy} {city}',
+			'search_field': 'name',
+			'per_page': 100
+		}
+		response = requests.get(url, params=params)
+		return response.json()['items']
